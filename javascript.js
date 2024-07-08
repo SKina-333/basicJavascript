e9cea75bd3db4c62f6991154e4d1d4f4cb097506
Sokina Ho <shoo0042@student.monash.edu>
2024-07-08 23:02:30 +1000

    added getComputerChoice method

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+
+function getComputerChoice () {
+    let RandNum = Math.floor(Math.random() * 3)
+
+    if (RandNum == 1) {
+        console.log("Rock");
+    }
+    else if (RandNum == 2) {
+        console.log("Paper");
+    }
+    else if (RandNum == 3) {
+        console.log("Scissor");
+    }
+}
+
+function getHumanChoice () {
+    
+}
